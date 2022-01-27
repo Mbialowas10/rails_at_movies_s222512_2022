dbfdca2074bcd37bcbbd7a5af117a7c94e1f339c
Michael Bialowas <mbialowas@rrc.ca>
2022-01-27 08:07:22 -0600

    Changes leading up to Jan. 27, 2022

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,10 @@
+class MoviesController < ApplicationController
+  def index
+    #@movies = Movie.order("average_VOTE DESC") # creates an n+1 issue
+    @movies = Movie.includes(:production_company).all.order("average_VOTE DESC")
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+end
