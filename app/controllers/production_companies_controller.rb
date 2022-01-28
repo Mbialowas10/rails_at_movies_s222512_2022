171b600f796f84e2994191ec2ad2bf2373057b71
Michael Bialowas <mbialowas@rrc.ca>
2022-01-28 10:59:18 -0600

    2022-01-28 snapshot - added partial for list item for production companies and movie collection

--- app/controllers/production_companies_controller.rb
+++ app/controllers/production_companies_controller.rb
@@ -1,7 +1,19 @@
 class ProductionCompaniesController < ApplicationController
   def index
     # fetch all production companies
-    @production_companies = ProductionCompany.all
+    # SELECT "production_companies"."id", "production_companies"."name", count(production_companies.id) as 'Movie Count' from "production_companies"
+    # LEFT OUTER JOIN "movies" on
+    # "movies"."production_company_id" = "production_companies"."id"
+    # GROUP BY "production_companies"."id"
+    # order by count(production_companies.id) desc
+
+    # @production_companies = ProductionCompany.select("production_companies.*")
+    #                                                             .select("COUNT(production_companies.id) as movie_count")
+    #                                                             .left_joins(:movies)
+    #                                                             .group("production_companies.id")
+    #                                                             .order("movie_count DESC")
+
+    @production_companies = ProductionCompany.ordered_by_movies
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'home/index'
   # get 'production_companies/index'
   # get 'production_companies/show'
   #get 'movies/index'
   #get 'movies/show'
   resources :movies, only: [:index, :show]
   resources :production_companies, only: [:index, :show]
+
+  #default route to website aka home page route
+  root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
